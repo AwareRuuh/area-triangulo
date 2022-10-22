a84d21a195aa2483cc89cdc454c7cb1264342259
Ruth <88747761+AwareRuuh@users.noreply.github.com>
2022-10-22 18:16:39 +0000

    calcula a área de um triângulo

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+  Scanner sc1 = new Scanner(System.in);
+   Double B,h,A;
+        System.out.println("Informe o valor da base: ");
+        B = sc1.nextDouble();
+        System.out.println("Informe o valor da altura: ");
+        h = sc1.nextDouble();
+  
+        A = (B*h)/2;
+        System.out.println("O valor que você inseriu é B: "+B+" h: "+h);
+        System.out.println("A área do triângulo é: "+A);
+        sc1.close();
+    }   
 }
